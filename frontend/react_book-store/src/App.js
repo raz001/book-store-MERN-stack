7f7fe2746915e5a711452e950966d972cf365d99
raz001 <razbhagat1@gmail.com>
2023-09-07 11:29:58 +0530

    crud operations added

--- frontend/react_book-store/src/App.js
+++ frontend/react_book-store/src/App.js
@@ -1,10 +1,21 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
+import React from 'react';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+import ShowBook from './pages/ShowBook';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+
+    </Routes>
   )
 }
 
---
+++ frontend/react_book-store/src/pages/DeleteBook.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton';
+import Spinner from '../components/BackButton';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:3000/books/${id}`)
+      .then((res) => {
+        setLoading(false)
+        alert(res.data.msg)
+        navigate('/')
+      })
+      .catch((err) => {
+        setLoading(false);
+
+      })
+  }
+  return (
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 ronded-xl w-[600px] p-4 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You Want to delete this book?</h3>
+        <button className='p-4 bg-red-400 text-white m-8 w-full' onClick={handleDeleteBook}>
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteBook
